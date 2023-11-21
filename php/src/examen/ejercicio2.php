5e75454bf2fff2afb6ddd79d6221fb0d36257e54
Andreu Puchades <andpucpas@alu.edu.gva.es>
2023-11-21 20:25:07 +0100

    segundo commit

--- php/src/examen/ejercicio2.php
+++ php/src/examen/ejercicio2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercici 2 - BatoiBook</title>
+    <title>Exercici 2</title>
     <style>
         table {
             width: 50%;
